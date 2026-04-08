7f8b4ca0e9537813f20c006370d423dc4e54adea
aeg <alireza.almer@gmail.com>
2026-04-08 14:12:35 +0200

    fix: skip availability check for GFW_TOKEN

--- apps/backend/src/config/api.ts
+++ apps/backend/src/config/api.ts
@@ -3,9 +3,9 @@ import dotenv from '@dotenvx/dotenvx';
 dotenv.config();
 import { IBackendConfig } from '../helpers/types/generalTypes';
 
-if (!process.env.GFW_TOKEN || process.env.GFW_TOKEN.length === 0) {
+/* if (!process.env.GFW_TOKEN || process.env.GFW_TOKEN.length === 0) {
   throw new Error("GFW_TOKEN is required. For more information, please refer to docs/runbook.md." )
-}
+} */
 
 export const config: IBackendConfig = {
   logging: {
